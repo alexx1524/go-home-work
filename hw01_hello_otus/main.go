6bf3b7def0c58d4736a0d41af0a1cc5405bfed00
Aleksei Bolelov <aleksei.bolelov@redlab.dev>
2022-04-28 22:47:03 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	data := "Hello, OTUS!"
+
+	fmt.Println(stringutil.Reverse(data))
 }
