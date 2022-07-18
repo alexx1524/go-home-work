49d33f7b19f51fd7cdbc1652ebfb9fd0703c79c0
Bolelov Aleksei <aleksei.bolelov@redlab.dev>
2022-07-18 22:00:16 +0300

    Reader and executor with tests were added

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
 	Value      string
 	NeedRemove bool
@@ -11,6 +18,59 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(Environment)
+
+	for _, file := range files {
+		fileName := file.Name()
+		value, err := getValue(path.Join(dir, fileName))
+		if err != nil {
+			return nil, err
+		}
+
+		result[fileName] = value
+	}
+
+	return result, err
+}
+
+func getValue(fileName string) (EnvValue, error) {
+	result := EnvValue{}
+
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		return result, err
+	}
+	size := fileInfo.Size()
+	if size == 0 {
+		result.NeedRemove = true
+		return result, nil
+	}
+
+	readFile, err := os.Open(fileName)
+	defer readFile.Close()
+
+	if err != nil {
+		return result, err
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	if fileScanner.Scan() {
+		line := fileScanner.Text()
+		line = strings.TrimRight(line, string(rune(0x00)))
+		line = strings.TrimRight(line, "\t")
+		line = strings.TrimRight(line, " ")
+		line = strings.ReplaceAll(line, "\x00", "\n")
+
+		if line == "" {
+			result.NeedRemove = true
+			return result, nil
+		}
+		result.Value = line
+	}
+
+	return result, nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("If command is not found then RunCmd returns error code", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/echo2.sh",
+		}
+
+		returnCode := RunCmd(cmd, nil)
+
+		require.Equal(t, returnCode, 127)
+	})
+
+	t.Run("If running data is correct then RunCmd returns 0 code", func(t *testing.T) {
+		data := []string{
+			"/bin/bash",
+			"testdata/echo.sh",
+		}
+
+		returnCode := RunCmd(data, nil)
+
+		require.Equal(t, returnCode, 0)
+	})
 }
