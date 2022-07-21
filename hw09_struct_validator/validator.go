a316be6ffb93421723fb6a1da69cc313dc04a3ac
Aleksei Bolelov <aleksei.bolelov@redlab.dev>
2022-07-21 10:06:29 +0300

    HW09 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,14 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const validationTag = "validate"
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -7,11 +16,73 @@ type ValidationError struct {
 
 type ValidationErrors []ValidationError
 
-func (v ValidationErrors) Error() string {
-	panic("implement me")
+func (errors ValidationErrors) Error() string {
+	var sb strings.Builder
+	for _, err := range errors {
+		sb.WriteString(fmt.Sprintf("Field:%s Error:%s \n", err.Field, err.Err.Error()))
+	}
+	return sb.String()
 }
 
+var (
+	ErrNotStruct                 = errors.New("input parameter is not struct")
+	ErrUnsupportedValidationType = errors.New("unsupported validation type")
+	ErrWrongValidatorFormat      = errors.New("wrong validation format")
+)
+
 func Validate(v interface{}) error {
-	// Place your code here.
+	value := reflect.ValueOf(v)
+	valueType := value.Type()
+
+	if value.Kind() != reflect.Struct {
+		return ErrNotStruct
+	}
+
+	result := make(ValidationErrors, 0)
+
+	for i := 0; i < value.Type().NumField(); i++ {
+		field := valueType.Field(i)
+		validation := field.Tag.Get(validationTag)
+
+		if validation == "" {
+			continue
+		}
+
+		var err error
+		switch field.Type.Kind() {
+		case reflect.Int:
+			err = validateInt(field.Name, int(value.Field(i).Int()), validation, &result)
+		case reflect.String:
+			err = validateString(field.Name, value.Field(i).String(), validation, &result)
+		case reflect.Slice:
+			err = validateSlice(field.Name, value.Field(i), validation, &result)
+		case reflect.Struct:
+			err = validateNestedStruct(value.Field(i), validation, &result)
+		default:
+			return ErrUnsupportedValidationType
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	return result
+}
+
+func validateNestedStruct(value reflect.Value, validationRules string, validationErrors *ValidationErrors) error {
+	if validationRules == "nested" {
+		err := Validate(value.Interface())
+		var errs ValidationErrors
+		if errors.As(err, &errs) {
+			*validationErrors = append(*validationErrors, errs...)
+		} else {
+			return err
+		}
+	}
 	return nil
 }
---
+++ hw09_struct_validator/validators.go
@@ -0,0 +1,152 @@
+package hw09structvalidator
+
+import (
+	"errors"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrLessThanMinimum     = errors.New("the value is less than the minimum allowed")
+	ErrGreaterThanMaximum  = errors.New("the value is greater than the maximum allowed")
+	ErrValueIsNotAllowed   = errors.New("the value is not allowed")
+	ErrInvalidStringLength = errors.New("invalid string length")
+	ErrWrongFormat         = errors.New("wrong format")
+)
+
+func parseValidators(validationRules string) (map[string]string, error) {
+	validators := strings.Split(validationRules, "|")
+	result := make(map[string]string)
+	for _, validator := range validators {
+		parts := strings.Split(validator, ":")
+		if len(parts) < 2 || parts[1] == "" {
+			return nil, ErrWrongValidatorFormat
+		}
+		result[parts[0]] = parts[1]
+	}
+	return result, nil
+}
+
+func validateSlice(fieldName string, value reflect.Value, rule string, errors *ValidationErrors) error {
+	var err error
+	switch value.Interface().(type) {
+	case []int:
+		for _, item := range value.Interface().([]int) {
+			err = validateInt(fieldName, item, rule, errors)
+		}
+	case []string:
+		for _, item := range value.Interface().([]string) {
+			err = validateString(fieldName, item, rule, errors)
+		}
+	}
+	return err
+}
+
+func validateString(fieldName string, value string, validationRules string, errors *ValidationErrors) error {
+	validators, err := parseValidators(validationRules)
+	if err != nil {
+		return err
+	}
+	for key, val := range validators {
+		switch key {
+		case "len":
+			length, err := strconv.Atoi(val)
+			if err != nil {
+				return err
+			}
+			if len(value) != length {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrInvalidStringLength,
+				})
+			}
+		case "regexp":
+			isValid, err := regexp.Match(val, []byte(value))
+			if err != nil {
+				return err
+			}
+			if !isValid {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrWrongFormat,
+				})
+			}
+
+		case "in":
+			allowedValues := strings.Split(val, ",")
+			isAllowed := false
+			for _, item := range allowedValues {
+				if value == item {
+					isAllowed = true
+					break
+				}
+			}
+			if !isAllowed {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrValueIsNotAllowed,
+				})
+			}
+		}
+	}
+	return nil
+}
+
+func validateInt(fieldName string, value int, validationRules string, errors *ValidationErrors) error {
+	validators, err := parseValidators(validationRules)
+	if err != nil {
+		return err
+	}
+	for key, val := range validators {
+		switch key {
+		case "min":
+			minValue, err := strconv.Atoi(val)
+			if err != nil {
+				return err
+			}
+			if value < minValue {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrLessThanMinimum,
+				})
+			}
+		case "max":
+			maxValue, err := strconv.Atoi(val)
+			if err != nil {
+				return err
+			}
+			if value > maxValue {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrGreaterThanMaximum,
+				})
+			}
+		case "in":
+			values := strings.Split(val, ",")
+			allowedValues := make([]int, 0, len(values))
+			for _, item := range values {
+				allowedValue, err := strconv.Atoi(item)
+				if err != nil {
+					return err
+				}
+				allowedValues = append(allowedValues, allowedValue)
+			}
+			isAllowed := false
+			for _, item := range allowedValues {
+				if value == item {
+					isAllowed = true
+					break
+				}
+			}
+			if !isAllowed {
+				*errors = append(*errors, ValidationError{
+					Field: fieldName,
+					Err:   ErrValueIsNotAllowed,
+				})
+			}
+		}
+	}
+	return nil
+}
